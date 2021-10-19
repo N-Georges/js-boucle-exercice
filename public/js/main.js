40577ad369126257643c41998924426cc66412ca
georges <gf.nodari@hotmail.com>
2021-10-19 14:18:07 +0200

    project init

---
+++ public/js/main.js
@@ -0,0 +1,26 @@
+// exo2*
+// let nom =["georges", "jo", "kev", "phil"]
+
+// for (let index = 0; index < nom.length; index++) {
+//     console.log(`bonjour ${nom[index]}`);    
+// }
+
+// exo3
+// let number = prompt("entre un nombre")
+// for (let index = 0; index < number.length; index++) {  
+//     console.log(number(index)); 
+// }
+
+
+// foreach
+// exo1
+// let fruit=["pomme", "kiwi", "peche", "raisin", "bannane"]
+// fruit.forEach(el => {
+//     console.log(el);
+// });
+
+// // exo3
+let classe = ["wa", "jo", "phil", "mo", "za", "hil"]
+classe.forEach((el, i) => {
+    console.log(`bonjour ${el}, ${i}`);
+});
---
+++ public/js/script.js
@@ -0,0 +1,50 @@
+// FOR
+//  1
+// for (let i = 0; i <= 20; i++) {
+//     console.log(i);
+// };
+
+// for (let i = 0; i <= 20; i++) {
+//     switch(i%2 == 0) {
+//         case true:
+//             console.log(`${i} est un nombre pair`);
+//             break;
+//         case false:
+//             console.log(`${i} est un nombre impair`);
+//             break;
+//     };  
+// };
+
+//2
+// let coding = ["saliou", "zakaria", "ayoub", "mohammed", "alex", "adame", "sebastien", "achraf", "bene", "abdellah", "malo", "louise", "natchez", "dawid", "maximilien", "jamila", "mexence", "zulma"]
+// console.log(coding);
+// for (let i = 0; i < coding.length; i++) {
+//     console.log(`bonjour ${coding[i]}`);
+    
+// }
+
+//3 
+// let nombre = prompt('un nombre')
+// for (let i = 0; i <= nombre; i++) {
+//     console.log(i);;
+    
+// }
+
+
+// FOREACH
+//1
+// let monTab  = ['molengeek', 'ayhan', 'bruxelles', 'voiture', 'frite']
+// monTab.forEach(element => {
+//     console.log(element);
+// });
+//2
+// let coding = ["saliou", "zakaria", "ayoub", "mohammed", "alex", "adame", "sebastien", "achraf", "bene", "abdellah", "malo", "louise", "natchez", "dawid", "maximilien", "jamila", "mexence", "zulma"]
+
+// ## sa position
+// coding.forEach(el => {
+//     console.log(`Bonjour ${el}`);
+// });
+
+// coding.forEach((e, i) => {
+//     console.log(`Element -> ${e}\n Position -> ${i}`);
+// });
